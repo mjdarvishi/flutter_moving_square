ef0d166997cfc04d391435b4aba4147d373ecd6f
mjdarvishi <mjdarivshi1374@gamil.com>
2023-01-04 19:04:21 +0330

    domain layer files are added

---
+++ lib/domain/entities/customer.dart
@@ -0,0 +1,13 @@
+
+class Customer {
+  final int id;
+  final String firstname;
+  final String lastname;
+  final String dateOfBirth;
+  final String phoneNumber;
+  final String email;
+  final String bankAccountNumber;
+
+  Customer(this.id, this.firstname, this.lastname, this.dateOfBirth,
+      this.phoneNumber, this.email, this.bankAccountNumber);
+}
