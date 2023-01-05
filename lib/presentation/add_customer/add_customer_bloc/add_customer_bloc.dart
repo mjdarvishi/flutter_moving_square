73bfd4237a37c1922509eb3764b1879d24ef7167
mjdarvishi <mjdarivshi1374@gamil.com>
2023-01-05 13:41:50 +0330

    add add customer state

--- lib/presentation/add_customer/add_customer_bloc/add_customer_bloc.dart
+++ lib/presentation/add_customer/add_customer_bloc/add_customer_bloc.dart
@@ -1,5 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:mc_crud_test/core/utils/validation/account.dart';
+import 'package:mc_crud_test/core/utils/validation/email.dart';
+import 'package:mc_crud_test/core/utils/validation/mobile.dart';
+import 'package:mc_crud_test/core/utils/validation/name.dart';
 
 part 'add_customer_event.dart';
 part 'add_customer_state.dart';
--- lib/presentation/add_customer/add_customer_bloc/add_customer_state.dart
+++ lib/presentation/add_customer/add_customer_bloc/add_customer_state.dart
@@ -1,13 +1,39 @@
 part of 'add_customer_bloc.dart';
 
 class AddCustomerState extends Equatable {
+  final Name? firstName;
+  final Name? lastName;
+  final Account? accountNumber;
+  final Email? email;
+  final Mobile? mobile;
+  final String dateOfBirth;
 
-@override
-List<Object?> get props => [];
+  @override
+  List<Object?> get props =>
+      [firstName, lastName, accountNumber, email, mobile, dateOfBirth];
 
-const AddCustomerState();
+  const AddCustomerState(
+      {this.dateOfBirth = '',
+      this.mobile,
+      this.email,
+      this.accountNumber,
+      this.lastName,
+      this.firstName});
 
-AddCustomerState copyWith() {
-return AddCustomerState();
-}
+  AddCustomerState copyWith({
+    Name? firstName,
+    Name? lastName,
+    Account? accountNumber,
+    Email? email,
+    Mobile? mobile,
+    String? dateOfBirth,
+  }) {
+    return AddCustomerState(
+        mobile: mobile ?? this.mobile,
+        email: email ?? this.email,
+        accountNumber: accountNumber ?? this.accountNumber,
+        dateOfBirth: dateOfBirth ?? this.dateOfBirth,
+        firstName: firstName ?? this.firstName,
+        lastName: lastName ?? this.lastName);
+  }
 }
