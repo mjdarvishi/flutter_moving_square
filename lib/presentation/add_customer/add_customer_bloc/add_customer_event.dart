dd8fe631409e74c40609545f37540b17a1b4c4f6
mjdarvishi <mjdarivshi1374@gamil.com>
2023-01-05 13:44:34 +0330

    add add customer events

--- lib/presentation/add_customer/add_customer_bloc/add_customer_event.dart
+++ lib/presentation/add_customer/add_customer_bloc/add_customer_event.dart
@@ -1,9 +1,44 @@
 part of 'add_customer_bloc.dart';
 
-abstract class AddCustomerEvent extends Equatable{
+abstract class AddCustomerEvent extends Equatable {
+  @override
+  List<Object> get props => [];
 
-@override
-List<Object> get props => [];
+  const AddCustomerEvent();
+}
 
-const AddCustomerEvent();
+class OnFirstNameChangeEvent extends AddCustomerEvent {
+  final String value;
+
+  const OnFirstNameChangeEvent(this.value);
+}
+
+class OnLastNameChangeEvent extends AddCustomerEvent {
+  final String value;
+
+  const OnLastNameChangeEvent(this.value);
+}
+
+class OnMobileChangeEvent extends AddCustomerEvent {
+  final String value;
+
+  const OnMobileChangeEvent(this.value);
+}
+
+class OnEmailChangeEvent extends AddCustomerEvent {
+  final String value;
+
+  const OnEmailChangeEvent(this.value);
+}
+
+class OnBackNumberChangeEvent extends AddCustomerEvent {
+  final String value;
+
+  const OnBackNumberChangeEvent(this.value);
+}
+
+class OnDateOfBirthNumberChangeEvent extends AddCustomerEvent {
+  final String value;
+
+  const OnDateOfBirthNumberChangeEvent(this.value);
 }
