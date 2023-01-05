0a7569c829ed12dedbf84759909373f265acda50
mjdarvishi <mjdarivshi1374@gamil.com>
2023-01-05 14:11:47 +0330

    complite text field validation in presenttion

--- lib/presentation/add_customer/view/add_customer.page.dart
+++ lib/presentation/add_customer/view/add_customer.page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mc_crud_test/core/utils/validation/account.dart';
+import 'package:mc_crud_test/core/utils/validation/email.dart';
+import 'package:mc_crud_test/core/utils/validation/mobile.dart';
+import 'package:mc_crud_test/core/utils/validation/name.dart';
 import 'package:mc_crud_test/injector.dart';
 import 'package:mc_crud_test/presentation/add_customer/add_customer_bloc/add_customer_bloc.dart';
 
 class AddCustomerPage extends StatelessWidget {
   const AddCustomerPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,29 +17,59 @@ class AddCustomerPage extends StatelessWidget {
           create: (context) => injector<AddCustomerBloc>(),
           child: BlocBuilder<AddCustomerBloc, AddCustomerState>(
             builder: (context, state) => Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-              child: Column(
-                children: [
-                  // First name
-                  customTextField((value) {}, '', 'err', 'First Name'),
-                  // Last name
-                  customTextField((value) {}, '', 'err', 'Last Name'),
-                  // PhoneNumber
-                  customTextField((value) {}, '', 'err', '0913*******'),
-                  // Email
-                  customTextField((value) {}, '', 'err', 'Email'),
-                  // BankAccountNumber
-                  customTextField((value) {}, '', 'err', 'BankAccountNumber'),
-                  OutlinedButton(
-                    onPressed: () => _selectDate(context),
-                    child: const Text('Select Date Of Birth'),
-                  ),
-                ],
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text('Add New Customer'),
+                    ),
+                    // First name
+                    customTextField(
+                        (value) => context
+                            .read<AddCustomerBloc>()
+                            .add(OnFirstNameChangeEvent(value)),
+                        state.firstName?.value??'',
+                        state.firstName?.error?.errDescription,
+                        'First Name'),
+                    // Last name
+                    customTextField(
+                            (value) => context
+                            .read<AddCustomerBloc>()
+                            .add(OnLastNameChangeEvent(value)),
+                        state.lastName?.value??'',
+                        state.lastName?.error?.errDescription, 'Last Name'),
+                    // MobileNumber
+                    customTextField(  (value) => context
+                        .read<AddCustomerBloc>()
+                        .add(OnMobileChangeEvent(value)),
+                        state.mobile?.value??'',
+                        state.mobile?.error?.errDescription, 'mobile like: 0913*******'),
+                    // Email
+                    customTextField(  (value) => context
+                        .read<AddCustomerBloc>()
+                        .add(OnEmailChangeEvent(value)),
+                        state.email?.value??'',
+                        state.email?.error?.errDescription, 'Email'),
+                    // BankAccountNumber
+                    customTextField(  (value) => context
+                        .read<AddCustomerBloc>()
+                        .add(OnBackNumberChangeEvent(value)),
+                        state.accountNumber?.value??'',
+                        state.accountNumber?.error?.errDescription, 'BankAccountNumber'),
+                    OutlinedButton(
+                      onPressed: () => _selectDate(context),
+                      child: const Text('Select Date Of Birth'),
+                    ),
+                  ],
+                ),
               ),
             ),
           )),
     );
   }
+
   Future<void> _selectDate(BuildContext context) async {
     final DateTime selectedDate = DateTime.now();
     final DateTime? picked = await showDatePicker(
@@ -43,46 +78,48 @@ class AddCustomerPage extends StatelessWidget {
         firstDate: DateTime(2015, 8),
         lastDate: DateTime(2101));
     if (picked != null && picked != selectedDate) {
-
+      print(picked);
     }
   }
 
-
   Widget customTextField(Function(String value) onChange, String initValue,
-      String err, String hint) {
-    return TextFormField(
-      onChanged: onChange,
-      initialValue: initValue,
-      decoration: InputDecoration(
-        errorText: err,
-        hintText: hint,
-        hintStyle: const TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.bold,
-          color: Colors.grey,
+      String ?err, String hint) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextFormField(
+        onChanged: onChange,
+        initialValue: initValue,
+        decoration: InputDecoration(
+          errorText: err,
+          hintText: hint,
+          hintStyle: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+          focusColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey, width: 1),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey, width: 1),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.red, width: 1),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.red, width: 1),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          errorStyle: const TextStyle(
+              color: Colors.red, fontWeight: FontWeight.bold, fontSize: 12),
+          fillColor: Colors.transparent,
         ),
-        focusColor: Colors.white,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 12),
-        filled: true,
-        enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.white, width: 1),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.white, width: 1),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.red, width: 1),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.red, width: 1),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        errorStyle: const TextStyle(
-            color: Colors.red, fontWeight: FontWeight.bold, fontSize: 12),
-        fillColor: Colors.transparent,
       ),
     );
   }
