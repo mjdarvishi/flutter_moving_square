9f54c96eafd7056fd2b3579270acf7fb3b078c0c
mjdarvishi <mjdarivshi1374@gamil.com>
2023-01-05 11:28:20 +0330

    set home state in home state file

--- lib/presentation/home/home_bloc/home_bloc.dart
+++ lib/presentation/home/home_bloc/home_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:mc_crud_test/domain/entities/customer.dart';
 
 part 'home_event.dart';
 part 'home_state.dart';
--- lib/presentation/home/home_bloc/home_state.dart
+++ lib/presentation/home/home_bloc/home_state.dart
@@ -1,13 +1,19 @@
 part of 'home_bloc.dart';
 
+enum HomePageState { loading, home, err }
+
 class HomeState extends Equatable {
+  final HomePageState state;
+  final List<Customer> customers;
 
-@override
-List<Object?> get props => [];
+  @override
+  List<Object?> get props => [state, customers];
 
-const HomeState();
+  const HomeState(
+      {this.state = HomePageState.loading, this.customers = const []});
 
-HomeState copyWith() {
-return HomeState();
-}
+  HomeState copyWith({HomePageState? state, List<Customer>? customers}) {
+    return HomeState(
+        state: state ?? this.state, customers: customers ?? this.customers);
+  }
 }
