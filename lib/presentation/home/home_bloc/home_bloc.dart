5b7dde73c0feb2bffd5b3f6b429ce9991e4bc57f
mjdarvishi <mjdarivshi1374@gamil.com>
2023-01-05 11:32:15 +0330

    add get customer method to home bloc

--- lib/presentation/home/home_bloc/home_bloc.dart
+++ lib/presentation/home/home_bloc/home_bloc.dart
@@ -3,12 +3,16 @@ import 'package:equatable/equatable.dart';
 import 'package:mc_crud_test/domain/entities/customer.dart';
 
 part 'home_event.dart';
+
 part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeState()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<GetCustomerEvent>(_getCustomers);
+  }
+
+  Future<void> _getCustomers(
+      GetCustomerEvent event, Emitter<HomeState> emit) async {
+
   }
 }
