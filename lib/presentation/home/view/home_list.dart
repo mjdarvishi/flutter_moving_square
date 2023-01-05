539598f3f697f18a99f20ccd546fc6984996ff5b
mjdarvishi <mjdarivshi1374@gamil.com>
2023-01-05 15:29:37 +0330

    add customer card

--- lib/presentation/home/view/home_list.dart
+++ lib/presentation/home/view/home_list.dart
@@ -8,7 +8,73 @@ class HomeList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = context.read<HomeBloc>();
-    return ListView.builder(itemCount: bloc.state.customers.length,
-      itemBuilder: (context, index) =>Container(),);
+    return ListView.builder(
+      itemCount: bloc.state.customers.length,
+      itemBuilder: (context, index) => Container(
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(color: Colors.blueGrey, width: 1)),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  customText(
+                      'First Name', bloc.state.customers[index].firstname),
+                  customText('Last Name', bloc.state.customers[index].lastname),
+                  customText(
+                      'Mobile Number', bloc.state.customers[index].phoneNumber),
+                  customText(
+                      'Email Address', bloc.state.customers[index].email),
+                  customText('Bank Account Number',
+                      bloc.state.customers[index].bankAccountNumber),
+                  customText(
+                      'Date Of Birth', bloc.state.customers[index].dateOfBirth),
+                ],
+              ),
+            ),
+            Column(
+              mainAxisSize: MainAxisSize.max,
+              children: const [
+                Icon(
+                  Icons.delete_sharp,
+                  color: Colors.red,
+                  size: 30,
+                ),
+                SizedBox(height: 25,),
+                Icon(
+                  Icons.edit_note_sharp,
+                  color: Colors.blue,
+                  size: 30,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget customText(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: RichText(
+        text: TextSpan(
+          text: title + ' : ',
+          style: const TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 15, color: Colors.black),
+          children: <TextSpan>[
+            TextSpan(
+                text: value,
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.blue)),
+          ],
+        ),
+      ),
+    );
   }
 }
